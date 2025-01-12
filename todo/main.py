317fce2410e5464fca47be9ffe6c3c91f6c926ef
developer-abhay <abhaysharma.developer@gmail.com>
2025-01-12 07:02:45 +0530

    feat: Persistent todos

--- todo/main.py
+++ todo/main.py
@@ -1,24 +1,25 @@
 import os
 import time
+import json
+import uuid
 
 # global list to store todos
 todos = [] # maybe make a type strict array
 
+# **************
 # clear terminal
-# clear terminal
-# clear terminal
+# **************
 def clear_terminal():
   if os.name == 'nt':
     os.system('cls') 
   else:
     os.system('clear')
 
-
+# *************
 # Get all Todos
-# Get all Todos
-# Get all Todos
-def getAllTodos():
-  clear_terminal() # clear the interval
+# *************
+def getAllTodos() -> None:
+  clear_terminal()
   
   if len(todos):
     print("All Todos:\n")
@@ -26,11 +27,14 @@ def getAllTodos():
       print(f"{index+1}. {x['task']} : {'done' if x['complete'] else 'pending'}")
     
     print("\n")
-    todoIndex = int(input("Which todo to update? (press '0' for menu) : "))
-  
+    try:
+      todoIndex = int(input("Which todo to update? (press '0' for menu) : "))
+    except:
+      todoIndex = 'invalid'
+
     if todoIndex == 0:
       return
-    elif todoIndex > len(todos):
+    elif todoIndex == 'invalid' or todoIndex > len(todos)  :
       print('\nInvalid input!!')
       time.sleep(1)
     else:
@@ -38,51 +42,84 @@ def getAllTodos():
   
   else:
     print('No Todos Available')
-    time.sleep(1)
+    time.sleep(0.8)
 
+# **************
 # Get Todo by Id
-# Get Todo by Id
-# Get Todo by Id
-def getTodoById(todoIndex):
+# **************
+def getTodoById(todoIndex:int) -> None:
   clear_terminal()
   todo = todos[todoIndex - 1]
   print(f"Task: {todo['task']} - {'Finished' if todo['complete'] else 'pending'}")
-  userInput = int(input(f"\n  1. Mark as {'pending' if todo['complete'] else 'Done'} \n  2. Delete Task \n\n Your choice (press '0' For Menu) : "))
+
+  try:
+    userInput = int(input(f"\n  1. Mark as {'pending' if todo['complete'] else 'Done'} \n  2. Delete Task \n\n Your choice (press '0' For Menu) : "))
+  except:
+    userInput = 'invalid'
   
-  updateTodo(todoIndex,userInput)
+  if userInput == 1 or userInput == 2:
+    updateTodo(todoIndex,userInput)
+  else:
+    print('\n  Invalid Input')
+    return
 
-
+# ********
 # Add Todo
-# Add Todo
-# Add Todo
-def addTodo():
+# ********
+def addTodo() -> None:
   clear_terminal()
+
   task = input("\n  Add Task: ")
-  todos.append({"id":len(todos) + 1,"task":task,"complete":False})
+  
+  todos.append({"id": str(uuid.uuid4()) ,"task":task,"complete":False})
+  
+  saveToJsonFile()
+  
   time.sleep(0.1)
   print("\n  Todo added successfully\n")
   time.sleep(0.6)
-  clear_terminal()
 
+# ******************
 # Update/Delete Todo
-# Update/Delete Todo
-# Update/Delete Todo
-def updateTodo(todoIndex, userInput):
+# ******************
+def updateTodo(todoIndex : int, userInput:int) -> None:
   todo = todos[todoIndex - 1]
   if userInput == 1:
     todo['complete'] = False if todo['complete'] else True
+    saveToJsonFile()
   elif userInput == 2:
     del todos[todoIndex - 1]
+    saveToJsonFile()
   else:
     print("\n Invalid Input!")
     time.sleep(0.6)
     return
+  
+# *****************
+# Save to Json file
+# *****************
+def saveToJsonFile():
+  with open('todos.json','w') as file:
+    json.dump(todos , file , indent=4)
 
+# *******************************
+# Load saved todos from json file
+# *******************************
+def loadTodosFromJson():
+  try:
+    with open('todos.json','r') as file:
+      return json.load(file)
+  except FileNotFoundError:
+    return []
+
+# *************
 # Main function
-# Main function
-# Main function
+# *************
 def main():
   clear_terminal() # clear the interval
+
+  global todos
+  todos = loadTodosFromJson()
   while True:
     try:
       clear_terminal() # clear the interval
